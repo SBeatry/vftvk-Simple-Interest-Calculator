080ca9778e47d191c0e5cb22010aa5271c659a19
SBeatry <77676484+SBeatry@users.noreply.github.com>
2021-01-29 16:10:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,29 @@ var n = d.getFullYear();
 var number = n + years;
 function slider1 ()
 {
- document.getElementById("percentage").innerHTML=this.value;
+document.getElementById("percentage").innerHTML=this.value;
+}
+
+
+function compute(){
+document.getElementById("result").innerHTML=
+"If you deposit principal," +
+"at an interest rate of rate." +
+"You will receive an amount of interest," +
+"in the year number";
+}
+
+
+
+function checkdata(){
+
+      //Check if principal is positive 
+      if(principal.value < 1){
+            alert("Enter a positive number");
+            principal.focus();
+            return false;
+      }
+     
+     //If it is positive return true.
+      return true;
 }
